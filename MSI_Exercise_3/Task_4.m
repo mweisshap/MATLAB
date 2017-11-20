ecef6113eff998fffcdc3d1a44ae3044b1f07312
mweisshap <33624317+mweisshap@users.noreply.github.com>
2017-11-20 11:17:11 +0100

    Fixed scaling issue
    Fixed the scaling issue, added Task b to e

--- MSI_Exercise_3/Task_4.m
+++ MSI_Exercise_3/Task_4.m
@@ -6,17 +6,21 @@ close all;
 clc;
 
 load('exercise3_data.mat');
-u(:,1)=data(:,1);
-i(:,1)=data(:,2);
+
+scaling = 1000;
+u(:,1)=data(:,1)./scaling;
+i(:,1)=data(:,2)./scaling;
 
 
 
 %Prepare the figure with labels
 figure('Name', 'Plot of Data');
 title('Plot of I_d over U');
-xlabel('U [mV]');
-ylabel('I_d [mA]');
-axis([min(u)-50 max(u)+50 min(i)-200 max(i)+200]);
+xlabel('U [V]');
+ylabel('I_d [A]');
+xpadding = 0.05;
+ypadding = 0.2;
+axis([min(u)-xpadding max(u)+xpadding min(i)-ypadding max(i)+ypadding]);
 grid on;
 hold on;
 
@@ -29,7 +33,7 @@ plot(u, i,'x')
 m = 1.5; %Ideality factor [Unitless]
 q = 1.6022e-19; %Elementary charge [C]
 %k_b = 1.3806e-23; %Boltzmann constant [J/K]
-k_b = physconst('Boltzmann');
+k_b = physconst('Boltzmann');%Boltzmann constant [J/K]
 T = 293; %Temperature [K]
 
 %Script Parameters
@@ -40,23 +44,40 @@ d = 2;%Dimension of Fit
 %Doing the LLS thing
 PHI=ones(N*n_p,d);
 
+
 %Fill in the values of the thing
+C = (exp((q*u(:))/(m*k_b*T))-1);
+
 for it=1:d
-    C=(exp((q*u(:))/(m*k_b*T))-1);
-    PHI(:,it)=u(1:(N*n_p)).^(it-1);
+   
+    %PHI(:,it)=u(1:(N*n_p)).^(it-1);
+    PHI(:, it) = C.^(it-1);
 end
+
+PHI=fliplr(PHI);
 
 theta = pinv(PHI) * i(1:N*n_p);
 
-x_p = 0:10:850;%mV
-y_p = (theta(1) + theta(2) .* (exp((q*x_p./1000)/(m*k_b*T))-1))
+%theta_2 = (inv(PHI'*PHI)*PHI') * i(1:N*n_p)
+
+x_p = 0:0.01:0.85;%V
+
+%y_p = (theta(1) + theta(2) * (exp((q*x_p)/(m*k_b*T))-1));
+
+y_p = (theta(2) + theta(1) * (exp((q*u)/(m*k_b*T))-1));
+
 
 hold on 
-plot(x_p, y_p);
-%{
-% compute inverse
-theta = (phi'* phi) \ phi' * I_d(1:N);
+plot(sort(u), sort(y_p));
 
-fit = theta(1) * (exp(q * U_k(1:N) / (m*k_B*T)) -1) + theta(2);
-%}
+legend('Data', 'Fit', 'Location', 'Northwest');
+
+error = i()-y_p();
+v_err = var(error);
+
+
+%c1 = cov(theta)
+c2 = v_err*inv(PHI'*PHI)
+c3 = (PHI'*PHI)/v_err
+
 
