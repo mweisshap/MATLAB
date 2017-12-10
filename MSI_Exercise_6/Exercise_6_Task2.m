5848f7321532d0a14021c27d8d9f65dc9775d80c
mweisshap <33624317+mweisshap@users.noreply.github.com>
2017-12-10 12:37:59 +0100

    Reduced objective function to only non-constant terms

--- MSI_Exercise_6/Exercise_6_Task2.m
+++ MSI_Exercise_6/Exercise_6_Task2.m
@@ -21,7 +21,7 @@ data_corrected=data_check(4:end)
 min(D.F)
 
 %histogram(data_check)
-h=histogram(data_corrected)
+h=histogram(data_corrected,'BinWidth',1)
 %x(2) = lambda
 %x(1) = k
 N=numel(data_corrected);
@@ -29,10 +29,16 @@ obj_fun=@(in)(-N*log(in(1))+N*in(1)*log(in(2))+1/((in(2)).^in(1))*sum(data_corre
 
 x=fmincon(obj_fun, [1 1],[],[],[],[],[0 0],[])
 
+obj_fun_2=@(in)1/((in(2)).^in(1))*sum(data_corrected.^in(1))-(in(1)-1)*sum(log(data_corrected));
+
+x_2=fmincon(obj_fun, [1 1],[],[],[],[],[0 0],[])
+
+
+
 d_f=makedist('Weibull','a',x(2),'b',x(1));
 y_f=N*pdf(d_f,h.BinEdges);
 hold on;
-plot(h.BinEdges,y_f)
+plot(h.BinEdges,y_f,'-x')
 
 
 %p_y=N*(x(1)/x(2))*(x_f/x(2)).^(x(1)-1).*exp(-((x_f/x(2)).^x(1)))
