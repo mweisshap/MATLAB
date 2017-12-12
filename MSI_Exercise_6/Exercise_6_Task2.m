5e46158cf967c1eb9a3559af1c8bab8dc13de797
mweisshap <33624317+mweisshap@users.noreply.github.com>
2017-12-12 19:37:30 +0100

    Polished everything a bit. This should be the final thing

--- MSI_Exercise_6/Exercise_6_Task2.m
+++ MSI_Exercise_6/Exercise_6_Task2.m
@@ -13,22 +13,35 @@ D=T(401233:(401233+8480),:);
 
 Data=D(:,4:5);
 
-F=Data.F%Wind Speed in m/s
+F=Data.F;%Wind Speed in m/s
 
 
 data_check=sort(F);
-data_corrected=data_check(4:end)
-min(D.F)
+data_corrected=data_check(4:end);
+min(D.F);
+
+%Make a nice plot
+figure('Name', 'Wind distribution');
+title('Histogram of wind distribution');
+ylabel('Occurrences');
+xlabel('wind speed [m/s]');
+grid on;
+hold on;
+
+
 
 %histogram(data_check)
-h=histogram(data_corrected,'BinWidth',1)
+h=histogram(data_corrected,'BinWidth',1);
 %x(2) = lambda
 %x(1) = k
 N=numel(data_corrected);
-obj_fun=@(in)(-N*log(in(1))+N*in(1)*log(in(2))+1/((in(2)).^in(1))*sum(data_corrected.^in(1))-(in(1)-1)*sum(log(data_corrected)));
 
+%Original objective function to minimize
+obj_fun=@(in)(-N*log(in(1))+N*in(1)*log(in(2))+1/((in(2)).^in(1))*sum(data_corrected.^in(1))-(in(1)-1)*sum(log(data_corrected)));
 x=fmincon(obj_fun, [1 1],[],[],[],[],[0 0],[])
 
+
+%Reduced objective function to minimize
 obj_fun_2=@(in)1/((in(2)).^in(1))*sum(data_corrected.^in(1))-(in(1)-1)*sum(log(data_corrected));
 
 x_2=fmincon(obj_fun, [1 1],[],[],[],[],[0 0],[])
@@ -36,23 +49,17 @@ x_2=fmincon(obj_fun, [1 1],[],[],[],[],[0 0],[])
 
 
 d_f=makedist('Weibull','a',x(2),'b',x(1));
-
 y_prb=pdf(d_f,h.BinEdges);
 
 %Power Curve
-pwr=[0 0 3 25 82 174 321 532 815 1180 1580 1900 2200 2480 2700 2850 2950 3020 3020 3020 3020 3020 3020 3020 3020 3020 0 0 0 0]
+pwr=[0 0 3 25 82 174 321 532 815 1180 1580 1900 2200 2480 2700 2850 2950 3020 3020 3020 3020 3020 3020 3020 3020 3020 0 0 0 0];
+%Computation of the powerdistribution
+powerdist = y_prb .* pwr;
 
-
-
-powerdist = y_prb .* pwr
-
+%Calculation of Expectation value for output power
 Exp_Power=trapz(powerdist)
 
 y_f=N*pdf(d_f,h.BinEdges);
 hold on;
-plot(h.BinEdges,y_f,'-x')
+plot(h.BinEdges,y_f,'-x','LineWidth', 1.25)
 
-
-
-
-%p_y=N*(x(1)/x(2))*(x_f/x(2)).^(x(1)-1).*exp(-((x_f/x(2)).^x(1)))
